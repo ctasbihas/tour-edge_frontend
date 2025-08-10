db80ca62c5eca58c14c7a369d858356f68cc48b3
ctasbihas <ctasbihas@gmail.com>
2025-08-11 02:08:57 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<main>
+			<h1>This is the main component</h1>
+		</main>
+	);
+}
+
+export default App;
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router";
+
+const routes = createBrowserRouter([
+	{
+		Component: App,
+		path: "/",
+	},
+]);
+
+export default routes;
