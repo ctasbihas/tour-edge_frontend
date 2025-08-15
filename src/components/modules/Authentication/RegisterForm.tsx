28971914a7543d9bf6db2a37da93d29215fe9b69
ctasbihas <ctasbihas@gmail.com>
2025-08-15 13:06:10 +0600

    feat: implement Register page with form validation and social login options

---
+++ src/components/modules/Authentication/RegisterForm.tsx
@@ -0,0 +1,179 @@
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import SocialButtons from "./SocialLoginButtons";
+
+const formSchema = z
+	.object({
+		name: z
+			.string()
+			.trim()
+			.min(3, "Name must be at least 3 characters long"),
+		email: z
+			.string()
+			.trim()
+			.min(1, "Email is required")
+			.email("Invalid email format"),
+		password: z
+			.string()
+			.min(1, "Password is required")
+			.min(8, "Password must be at least 8 characters long")
+			.regex(/(?=.*[a-z])/, {
+				message: "Password must contain at least one lowercase letter",
+			})
+			.regex(/(?=.*[A-Z])/, {
+				message: "Password must contain at least one uppercase letter",
+			})
+			.regex(/(?=.*[!@#$%^&*()])/, {
+				message:
+					"Password must contain at least one special character (!@#$%^&*())",
+			})
+			.regex(/(?=.*\d)/, {
+				message: "Password must contain at least one number",
+			}),
+		confirmPassword: z.string().min(1, "Please confirm your password"),
+	})
+	.refine((v) => v.password === v.confirmPassword, {
+		path: ["confirmPassword"],
+		message: "Passwords do not match",
+	});
+
+type FormValues = z.infer<typeof formSchema>;
+
+const RegisterForm = () => {
+	const form = useForm<FormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+		},
+		mode: "onSubmit",
+	});
+
+	const onSubmit = async (data: FormValues) => {
+		// TODO: Add register API
+		console.log("Form submitted:", data);
+		form.reset();
+	};
+
+	return (
+		<>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="space-y-8"
+					noValidate
+				>
+					<FormField
+						control={form.control}
+						name="name"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Name</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="John Doe"
+										autoComplete="name"
+										{...field}
+									/>
+								</FormControl>
+								<FormDescription className="sr-only">
+									This is your name.
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input
+										type="email"
+										placeholder="example@domain.com"
+										autoComplete="email"
+										{...field}
+									/>
+								</FormControl>
+								<FormDescription className="sr-only">
+									This is your email address.
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Password</FormLabel>
+								<FormControl>
+									<Input
+										type="password"
+										placeholder="••••••••"
+										autoComplete="new-password"
+										{...field}
+									/>
+								</FormControl>
+								<FormDescription className="sr-only">
+									This is your super secure password.
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="confirmPassword"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Confirm Password</FormLabel>
+								<FormControl>
+									<Input
+										type="password"
+										placeholder="••••••••"
+										autoComplete="new-password"
+										{...field}
+									/>
+								</FormControl>
+								<FormDescription className="sr-only">
+									Confirm your password.
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button
+						type="submit"
+						className="w-full"
+						disabled={form.formState.isSubmitting}
+					>
+						{form.formState.isSubmitting
+							? "Registering..."
+							: "Register"}
+					</Button>
+				</form>
+			</Form>
+			<SocialButtons />
+		</>
+	);
+};
+
+export default RegisterForm;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,8 +1,45 @@
+import SlideImage from "@/assets/login.png";
+import RegisterForm from "@/components/modules/Authentication/RegisterForm";
+import { Card, CardContent } from "@/components/ui/card";
+
 const Register = () => {
 	return (
-		<div>
-			<h1>Hello, src/pages/register.tsx</h1>
-		</div>
+		<main className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+			<div className="w-full max-w-sm md:max-w-3xl">
+				<div className="flex flex-col gap-6">
+					<Card className="overflow-hidden p-0">
+						<CardContent className="grid p-0 md:grid-cols-2">
+							<div className="bg-muted relative hidden md:block">
+								<img
+									src={SlideImage}
+									alt="Image"
+									className="absolute inset-0 h-full w-full object-contain dark:bg-lime-950 bg-lime-100"
+								/>
+							</div>
+							<div className="p-6 md:p-8">
+								<div className="flex flex-col gap-6">
+									<div className="flex flex-col items-center text-center">
+										<h1 className="text-2xl font-bold">
+											Welcome to Tour Edge
+										</h1>
+										<p className="text-muted-foreground text-balance">
+											Join us and explore the world of
+											travel.
+										</p>
+									</div>
+									<RegisterForm />
+								</div>
+							</div>
+						</CardContent>
+					</Card>
+					<div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+						By clicking continue, you agree to our{" "}
+						<a href="">Terms of Service</a> and{" "}
+						<a href="">Privacy Policy</a>.
+					</div>
+				</div>
+			</div>
+		</main>
 	);
 };
 
